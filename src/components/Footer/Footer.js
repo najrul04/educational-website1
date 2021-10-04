6574e4455df30111147a24fe1593778d873be93c
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2021-10-04 20:55:59 +0600

    header and Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,60 @@
-import React from 'react';
-import { Badge } from 'react-bootstrap';
-
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 const Footer = () => {
-    return (
-            <div className='bg-secondary pb-2'>
-        <h1>Phone: 800-123-456 | Email: contact@example.com</h1>
-        <h1>Copyright © 2021 Mia Bacchu</h1>  
+  return (
+    <div classNameName="footer">
+      <div className="row">
+        <div className="col-md-12">
+          <footer className="footer">
+            <Container>
+              <div className="row">
+                <div className="col-md-3 m-b-30">
+                  <div className="footer-title m-t-5 m-b-20 p-b-8">
+                    About us
+                  </div>
+                  <p className="white-text">
+                    Here we will make sure that you find the best football academy for your kid so that your kids could make you proud someday.
+                  </p>
+                </div>
+                <div className="col-md-3 m-b-30">
+                  <div className="footer-title m-t-5 m-b-20 p-b-8">
+                    Admission
+                  </div>
+                  <div className="footer-links">
+                    <Link to="#">Policy</Link>
+                    <Link to="#">FAQ</Link>
+                    <Link to="#">Admission Form</Link>
+                  </div>
+                </div>
+                <div className="col-md-3 m-b-30">
+                  <div className="footer-title m-t-5 m-b-20 p-b-8">
+                    Department
+                  </div>
+                  <div className="footer-links">
+                    <Link to="#">Under 9-14</Link>
+                    <Link to="#">Under -16</Link>
+                    <Link to="#">Under - 21</Link>
+                  </div>
+                </div>
+                <div className="col-md-3 m-b-30">
+                  <div className="footer-title m-t-5 m-b-20 p-b-8">Support</div>
+                  <div className="footer-links">
+                    <Link to="#">Equipment</Link>
+                    <Link to="#">About Us</Link>
+                    <Link to="#">Contact Us</Link>
+                  </div>
+                </div>
+              </div>
+            </Container>
+          </footer>
+          <div className="footer-bottom">
+            Copyright © 2021 All Rights Reserved
+          </div>
         </div>
-        
-    );
+      </div>
+    </div>
+  );
 };
-
 export default Footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,13 +5,15 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
     return (
         <Navbar bg="dark" variant="dark">
-    <Container className='py-3'>
+    <Container className='py-1'>
     <Navbar.Brand className='text-info fs-2'>Football School</Navbar.Brand>
     <Nav className="me-auto">
+
       <NavLink  className='m-2 text-white text-decoration-none fs-3' to="/home" >Home</NavLink>
       <NavLink  className='m-2 text-white text-decoration-none fs-3' to="/services">Services</NavLink>
       <NavLink  className='m-2 text-white text-decoration-none fs-3' to="/about">About Us</NavLink>
       <NavLink  className='m-2 text-white text-decoration-none fs-3' to="/contact">Contact Us</NavLink>
+
     </Nav>
     </Container>
   </Navbar>
